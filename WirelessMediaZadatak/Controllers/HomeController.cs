b390287b5de58f8d23240b1d643acbf81caad39a
Filip Panic <filip.panic.1993@gmail.com>
2018-08-04 22:06:21 +0200

    Prikaz proizvoda

---
+++ WirelessMediaZadatak/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WirelessMediaZadatak.Models;
+
+namespace WirelessMediaZadatak.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            WirelessMediaDbEntities entities = new WirelessMediaDbEntities();
+            IEnumerable<Proizvod> listaProizvoda = entities.Proizvodi;
+            return View(listaProizvoda.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
