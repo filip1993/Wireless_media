eaa6b5f5a6c5203a41438611db61dc8ce70a2efb
Filip Panic <filip.panic.1993@gmail.com>
2018-08-05 16:48:37 +0200

    Kreirane strane za unos i izmenu proizvoda

--- WirelessMediaZadatak/Controllers/HomeController.cs
+++ WirelessMediaZadatak/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WirelessMediaZadatak.Models;
@@ -9,25 +11,69 @@ namespace WirelessMediaZadatak.Controllers
 {
     public class HomeController : Controller
     {
+        private WirelessMediaDbEntities db = new WirelessMediaDbEntities();
+
         public ActionResult Index()
         {
-            WirelessMediaDbEntities entities = new WirelessMediaDbEntities();
-            IEnumerable<Proizvod> listaProizvoda = entities.Proizvodi;
-            return View(listaProizvoda.ToList());
+            return View(db.Proizvodi.ToList());
         }
 
-        public ActionResult About()
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.DobavljacId = new SelectList(db.Dobavljaci, "DobavljacId", "NazivDobavljaca");
+            ViewBag.KategorijaId = new SelectList(db.Kategorije, "KategorijaId", "NazivKategorije");
+            ViewBag.ProizvodjacId = new SelectList(db.Proizvodjaci, "ProizvodjacId", "NazivProizvodjaca");
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProizvodId,KategorijaId,ProizvodjacId,DobavljacId,Naziv,Opis,Cena")] Proizvod proizvod)
         {
-            ViewBag.Message = "Your contact page.";
+            if (ModelState.IsValid)
+            {
+                db.Proizvodi.Add(proizvod);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-            return View();
+            ViewBag.DobavljacId = new SelectList(db.Dobavljaci, "DobavljacId", "NazivDobavljaca", proizvod.DobavljacId);
+            ViewBag.KategorijaId = new SelectList(db.Kategorije, "KategorijaId", "NazivKategorije", proizvod.KategorijaId);
+            ViewBag.ProizvodjacId = new SelectList(db.Proizvodjaci, "ProizvodjacId", "NazivProizvodjaca", proizvod.ProizvodjacId);
+            return View(proizvod);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proizvod proizvod = db.Proizvodi.Find(id);
+            if (proizvod == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DobavljacId = new SelectList(db.Dobavljaci, "DobavljacId", "NazivDobavljaca", proizvod.DobavljacId);
+            ViewBag.KategorijaId = new SelectList(db.Kategorije, "KategorijaId", "NazivKategorije", proizvod.KategorijaId);
+            ViewBag.ProizvodjacId = new SelectList(db.Proizvodjaci, "ProizvodjacId", "NazivProizvodjaca", proizvod.ProizvodjacId);
+            return View(proizvod);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProizvodId,KategorijaId,ProizvodjacId,DobavljacId,Naziv,Opis,Cena")] Proizvod proizvod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proizvod).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DobavljacId = new SelectList(db.Dobavljaci, "DobavljacId", "NazivDobavljaca", proizvod.DobavljacId);
+            ViewBag.KategorijaId = new SelectList(db.Kategorije, "KategorijaId", "NazivKategorije", proizvod.KategorijaId);
+            ViewBag.ProizvodjacId = new SelectList(db.Proizvodjaci, "ProizvodjacId", "NazivProizvodjaca", proizvod.ProizvodjacId);
+            return View(proizvod);
         }
     }
 }
